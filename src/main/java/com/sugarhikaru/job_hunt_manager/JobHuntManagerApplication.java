317560f1b700e15632364df433de08a8d5d5c1c3
SugerHikaru <hikaru.sato1203@icloud.com>
2026-05-26 22:54:14 +0900

    feat: initialize Spring Boot project

---
+++ src/main/java/com/sugarhikaru/job_hunt_manager/JobHuntManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sugarhikaru.job_hunt_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobHuntManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobHuntManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sugarhikaru/job_hunt_manager/JobHuntManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugarhikaru.job_hunt_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHuntManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
