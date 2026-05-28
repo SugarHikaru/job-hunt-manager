5c4a0f00bcf802669cb377e673683aeedcd7cca7
SugerHikaru <hikaru.sato1203@icloud.com>
2026-05-28 22:57:59 +0900

    feat: add COmpany controller

---
+++ src/main/java/com/sugarhikaru/job_hunt_manager/controller/CompanyController.java
@@ -0,0 +1,13 @@
+package com.sugarhikaru.job_hunt_manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CompanyController {
+  
+  @GetMapping("/companies")
+  public String showCompanyList() {
+    return "company-list";
+  }
+}
